241af552edddc2aa6cde97cfbe5059e69a64b3a4
Dmitry Shibanov <shibanov-1997@inbox.ru>
2020-02-20 12:51:38 +0300

    Update create-release.ps1

--- Images.CI/create-release.ps1
+++ Images.CI/create-release.ps1
@@ -6,20 +6,21 @@ param(
     [String] [Parameter (Mandatory = $True)] $DefinitionId,
     [String] [Parameter (Mandatory = $True)] $AccessToken
 )
-$Body = @{
-    definitionId = $DefinitionId
-    variables = {
-      ImageBuildId = {
-        value = $BuildId
-      }
-      ImageName = {
-        value = $ImageName
-      }
-    }
-    isDraft = "false"
-} | ConvertTo-Json
 
-Write-Host $($Body.Emit())
+$Body = @"
+{
+    "definitionId" : "$DefinitionId",
+    "variables" : {
+        "ImageBuildId" : {
+            "value" : "$BuildId"
+        },
+        "ImageName" : {
+            "value" : "$ImageName"
+        }
+    },
+    "isDraft" : "false"
+}
+"@
 $URL = "https://vsrm.dev.azure.com/$Organization/$Project/_apis/release/releases?api-version=5.1"
 
 $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes("'':${AccessToken}"))
@@ -27,21 +28,7 @@ $headers = @{
     Authorization = "Basic ${base64AuthInfo}"
 }
 
-$params = @{
-    Method = "POST"
-    ContentType = "application/json"
-    Uri = $URL
-    Headers = $headers
-    Body = $Body
-}
-
-$pipeline = Invoke-RestMethod -Uri $URL -Headers $headers
-$pipeline.value | ForEach-Object {
-  Write-Host $_ | Format-List | Out-String
-}
-
-$NewRelease = Invoke-RestMethod @params
+$NewRelease = Invoke-RestMethod $URL -Body $Body -Method "POST" -Headers $headers -ContentType "application/json"
 
 
-
-Write-Host "Created release: $($NewRelease.release._links.web.refs)"
+Write-Host "Created release: $($NewRelease)"
